89c0e8228f2d6b93f18450b647de40f03a10af85
pasandevin <jayawardene.pasandevin@gmail.com>
2023-07-12 17:46:18 +0530

    Refactor

--- src/main/java/EmployeeManagementUI.java
+++ src/main/java/EmployeeManagementUI.java
@@ -4,32 +4,26 @@ import java.util.Scanner;
 public class EmployeeManagementUI {
 
     private final EmployeeDAO employeeDAO;
+    private final Scanner scanner;
 
     public EmployeeManagementUI(EmployeeDAO employeeDAO) {
         this.employeeDAO = employeeDAO;
+        scanner = new Scanner(System.in);
     }
     public void start() {
-        int choice = 0;
 
-        while ( choice !=6) {
+        int choice;
+
+        do {
             printChoices();
-            Scanner scanner = new Scanner(System.in);
             choice = scanner.nextInt();
 
             if(choice == 1) {
-                System.out.println("Enter Employee ID");
-                int id = scanner.nextInt();
-                System.out.println("Enter Employee Name");
-                String name = scanner.next();
-                System.out.println("Enter Employee Age");
-                int age = scanner.nextInt();
-                System.out.println("Enter Employee Department");
-                String department = scanner.next();
-                Employee employee = new Employee(id, name, age, department);
+                Employee employee = getEmployeeDetailsFromUser();
                 employeeDAO.addEmployee(employee);
+
             } else if (choice == 2) {
-                System.out.println("Enter Employee ID");
-                int id = scanner.nextInt();
+                int id = getEmployeeIdFromUser();
                 try {
                     Employee employee = employeeDAO.getEmployeeById(id);
                     printEmployeeDetails(employee);
@@ -38,21 +32,15 @@ public class EmployeeManagementUI {
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
+
             } else if (choice == 3) {
-                System.out.println("Enter Employee ID");
-                int id = scanner.nextInt();
-                System.out.println("Enter Employee Name");
-                String name = scanner.next();
-                System.out.println("Enter Employee Age");
-                int age = scanner.nextInt();
-                System.out.println("Enter Employee Department");
-                String department = scanner.next();
-                Employee employee = new Employee(id, name, age, department);
+                Employee employee = getEmployeeDetailsFromUser();
                 employeeDAO.updateEmployee(employee);
+
             } else if (choice == 4) {
-                System.out.println("Enter Employee ID");
-                int id = scanner.nextInt();
+                int id = getEmployeeIdFromUser();
                 employeeDAO.deleteEmployee(id);
+
             } else if (choice == 5) {
                 try {
                     for (Employee employee : employeeDAO.getAllEmployees()) {
@@ -63,8 +51,10 @@ public class EmployeeManagementUI {
                 } catch (EmployeeNotFoundException e) {
                     e.printStackTrace();
                 }
+
             }
-        }
+
+        } while (choice != 6);
     }
 
     private void printChoices() {
@@ -76,10 +66,28 @@ public class EmployeeManagementUI {
         System.out.println("5. Get All Employees");
         System.out.println("6. Exit");
     }
+
     private void printEmployeeDetails(Employee employee) {
         System.out.println("Employee ID: " + employee.getId());
         System.out.println("Employee Name: " + employee.getName());
         System.out.println("Employee Age: " + employee.getAge());
         System.out.println("Employee Department: " + employee.getDepartment());
+    }
+
+    private Employee getEmployeeDetailsFromUser() {
+        int id = getEmployeeIdFromUser();
+        System.out.println("Enter Employee Name");
+        String name = scanner.next();
+        System.out.println("Enter Employee Age");
+        int age = scanner.nextInt();
+        System.out.println("Enter Employee Department");
+        String department = scanner.next();
+        return new Employee(id, name, age, department);
+    }
+
+    private int getEmployeeIdFromUser() {
+        System.out.println("Enter Employee ID");
+        int id = scanner.nextInt();
+        return id;
     }
 }
