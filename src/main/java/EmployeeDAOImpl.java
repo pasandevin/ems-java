60c1d5ea457bb6f3de3ba3233607df24821164fb
pasandevin <jayawardene.pasandevin@gmail.com>
2023-07-12 20:21:44 +0530

    Add more constants

--- src/main/java/EmployeeDAOImpl.java
+++ src/main/java/EmployeeDAOImpl.java
@@ -1,4 +1,4 @@
-import constants.SQLQueryConstants;
+import constants.DataAccessConstants;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -20,7 +20,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     public void addEmployee(Employee employee) {
 
-        String sql = SQLQueryConstants.INSERT_EMPLOYEE;
+        String sql = DataAccessConstants.INSERT_EMPLOYEE_QUERY;
 
         try {
             statement = dbConnection.prepareStatement(sql);
@@ -35,7 +35,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
             int rowsInserted = statement.executeUpdate();
 
             if (rowsInserted > 0) {
-                System.out.println("Employee added successfully!");
+                System.out.println(DataAccessConstants.INSERT_EMPLOYEE_SUCCESS_MSG);
             }
 
             statement.close();
@@ -48,7 +48,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     public Employee getEmployeeById(int employeeId) throws EmployeeNotFoundException, SQLException {
 
         // SQL query to retrieve an employee
-        String sql = SQLQueryConstants.GET_EMPLOYEE_BY_ID;
+        String sql = DataAccessConstants.GET_EMPLOYEE_BY_ID_QUERY;
 
         // Creating a prepared statement with the query
         statement = dbConnection.prepareStatement(sql);
@@ -63,13 +63,13 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
         // Processing the result set
         if (resultSet.next()) {
-            String name = resultSet.getString("name");
-            int age = resultSet.getInt("age");
-            String department = resultSet.getString("department");
+            String name = resultSet.getString(DataAccessConstants.EMPLOYEE_NAME_COLUMN);
+            int age = resultSet.getInt(DataAccessConstants.EMPLOYEE_AGE_COLUMN);
+            String department = resultSet.getString(DataAccessConstants.EMPLOYEE_DEPARTMENT_COLUMN);
             employee = new Employee(employeeId, name, age, department);
         } else {
             resultSet.close();
-            throw new EmployeeNotFoundException("No employee with the given id found in the database");
+            throw new EmployeeNotFoundException(DataAccessConstants.NO_EMPLOYEE_WITH_ID_ERR_MSG);
         }
         return employee;
     }
@@ -78,7 +78,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     public void updateEmployee(Employee employee) {
 
         // SQL query to update an employee
-        String sql = SQLQueryConstants.UPDATE_EMPLOYEE;
+        String sql = DataAccessConstants.UPDATE_EMPLOYEE_QUERY;
 
         try {
             // Creating a prepared statement with the query
@@ -93,9 +93,9 @@ public class EmployeeDAOImpl implements EmployeeDAO {
             // Executing the query
             int rowsUpdated = statement.executeUpdate();
             if (rowsUpdated > 0) {
-                System.out.println("Employee updated successfully!");
+                System.out.println(DataAccessConstants.UPDATE_EMPLOYEE_SUCCESS_MSG);
             } else {
-                System.out.println("No employee found with the specified ID.");
+                System.out.println(DataAccessConstants.NO_EMPLOYEE_WITH_ID_ERR_MSG);
             }
 
             // Closing the statement and connection
@@ -108,7 +108,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     public void deleteEmployee(int id) {
         // SQL query to delete an employee
-        String sql = SQLQueryConstants.DELETE_EMPLOYEE_BY_ID;
+        String sql = DataAccessConstants.DELETE_EMPLOYEE_BY_ID_QUERY;
 
         try {
             // Creating a prepared statement with the query
@@ -121,9 +121,9 @@ public class EmployeeDAOImpl implements EmployeeDAO {
             // Executing the query
             int rowsDeleted = statement.executeUpdate();
             if (rowsDeleted > 0) {
-                System.out.println("Employee deleted successfully!");
+                System.out.println(DataAccessConstants.DELETE_EMPLOYEE_SUCCESS_MSG);
             } else {
-                System.out.println("No employee found with the specified ID.");
+                System.out.println(DataAccessConstants.NO_EMPLOYEE_WITH_ID_ERR_MSG);
             }
             statement.close();
 
@@ -135,7 +135,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     public List<Employee> getAllEmployees() throws SQLException, EmployeeNotFoundException {
         // SQL query to retrieve all employees
-        String sql = SQLQueryConstants.GET_ALL_EMPLOYEES;
+        String sql = DataAccessConstants.GET_ALL_EMPLOYEES_QUERY;
 
         ArrayList<Employee> employees = new ArrayList<Employee>();
 
@@ -148,10 +148,10 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
             // Processing the result set
             while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                int age = resultSet.getInt("age");
-                String department = resultSet.getString("department");
+                int id = resultSet.getInt(DataAccessConstants.EMPLOYEE_ID_COLUMN);
+                String name = resultSet.getString(DataAccessConstants.EMPLOYEE_NAME_COLUMN);
+                int age = resultSet.getInt(DataAccessConstants.EMPLOYEE_AGE_COLUMN);
+                String department = resultSet.getString(DataAccessConstants.EMPLOYEE_DEPARTMENT_COLUMN);
                 Employee employee = new Employee(id, name, age, department);
                 employees.add(employee);
             }
@@ -165,7 +165,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         }
 
         if (employees.isEmpty()) {
-            throw new EmployeeNotFoundException("No employees found in the database");
+            throw new EmployeeNotFoundException(DataAccessConstants.ZERO_EMPLOYEES_ERR_MSG);
         }
 
         return employees;
